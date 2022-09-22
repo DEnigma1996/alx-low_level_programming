28ddb70dc73658d1c38085afb58204aa298ec727
Nwachukwu C. Emmanuel <chizimuzoemmanuel@gmail.com>
2022-09-22 17:53:49 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
-include "main.h"
+#include "main.h"
 
 /**
- ** main - check the code
+ * main - check the code
  *
- ** Return: Always 0.
- **/
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char word[9] = "_putchar";
-	int i;
-	for (i = 0; i < 8; i++)
-		_putchar(word[i]);
-	_putchar('\n');
-	return (0);
+char word[9] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+_putchar(word[i]);
+_putchar('\n');
+return (0);
 }
